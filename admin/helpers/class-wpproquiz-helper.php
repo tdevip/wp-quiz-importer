9c01445d57e6bf57de376c6a4aa36476417a756d
Prasad Tumula <tdevip@yahoo.com>
2017-08-12 10:12:06 +0100

    Included q_ans_correct attribute

--- admin/helpers/class-wpproquiz-helper.php
+++ admin/helpers/class-wpproquiz-helper.php
@@ -88,9 +88,11 @@ if ( !class_exists( 'wpqi_wpproquiz_helper' ) ) {
 	  			$value = $child->nodeValue;
 				if( !isset($value) ) continue;
 
-		  		//first answer is the correct answer
+		  		//check for correct answers
 		  		$isCorrect = 'no';
-		  		if(empty($answers)) $isCorrect = 'yes';
+		  		if($child->hasAttribute('q_ans_correct')) {
+		  			$isCorrect = ("yes" === $child->getAttribute("q_ans_correct")) ? "yes" : "no";
+		  		}
 
 				$answers[] = array(
 					'q_ans'  	=> $value,
