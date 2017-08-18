c3727fc9cc991ddb5412d2ee656760824edb8f10
Prasad Tumula <tdevip@yahoo.com>
2017-08-18 13:28:34 +0100

    Added instructions to plugin page. Modified template file to display macros on ribbon.

--- admin/partials/wp-quiz-importer-admin-display.php
+++ admin/partials/wp-quiz-importer-admin-display.php
@@ -55,4 +55,29 @@
 		 	<input type="submit" class="button-primary" value="<?php _e( 'Import' , 'wp-quiz-importer'); ?>" />
 		</p>
 	</form>
+
+	<table class="form-table">
+		<tr valign="top">
+			<th scope="row"><label for="wp_quiz_note"><?php _e( 'NOTE' , 'wp-quiz-importer'); ?></label></th>
+			<td>
+				<span name="wp_quiz_note" class="description"><?php echo __( 'Free version of the plugin supports two types (MC and MS) of questions only. Furthermore, it assigns “Default Points” to each correct answer if quiz provider does not support default points option.' , 'wp-quiz-importer'); ?></span>
+			</td>
+		</tr>
+		<tr valign="top">
+			<th scope="row"><label for="wp_quiz_instructions"><?php _e( 'Instructions' , 'wp-quiz-importer'); ?></label></th>
+			<td>
+				<div name="wp_quiz_instructions" class="quiz-import-instructions">
+					<ol>
+ 						<li>Open the MS word file and enable macros</li>
+ 						<li>If you want to insert a new category click <strong>Quiz Importer --&gt; New Category</strong> button on the menu. A default “[Category Name]” along with a question template will be inserted. You may change the name of the category if you wish.</li>
+ 						<li>If you want to insert a new question click <strong>Quiz Importer --&gt; New Question</strong> button on the menu. A new table will be inserted.</li>
+ 						<li>Enter data into the table in UTF-8 format (plain text) as explained in the example table above.</li>
+ 						<li>Please note that the text entered outside the table(s) will be ignored. Once data entry (all questions are prepared) is complete, click <strong>Quiz Importer --&gt; Export XML</strong>. This will create an XML file, which can later be imported into wordpress using WP Quiz Provider.</li>
+ 						<li>Each question must be entered in a separate table and few sample questions are provided below for reference.</li>
+ 						<li>See word template for complete instructions.</li>
+					</ol>
+				</div>
+			</td>
+		</tr>
+	</table>
 </div>
